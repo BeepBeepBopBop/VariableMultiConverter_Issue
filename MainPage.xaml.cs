69d0d3dd7f28ad9274276577e045de59f8f5cae3
BeepBeepBopBop <evan.carrere@gmail.com>
2024-07-20 12:16:52 +0200

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace MauiApp4
+{
+    public partial class MainPage : ContentPage
+    {
+        public static readonly BindableProperty ConditionAProperty = BindableProperty.Create(nameof(ConditionA), typeof(bool), typeof(MainPage));
+        public bool ConditionA
+        {
+            get => (bool)GetValue(ConditionAProperty);
+            private set => SetValue(ConditionAProperty, value);
+        }
+
+        public static readonly BindableProperty ConditionBProperty = BindableProperty.Create(nameof(ConditionB), typeof(bool), typeof(MainPage));
+        public bool ConditionB
+        {
+            get => (bool)GetValue(ConditionBProperty);
+            private set => SetValue(ConditionBProperty, value);
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
